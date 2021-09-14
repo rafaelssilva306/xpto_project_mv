444e63cd07f70f81cbefaeffaabd71b7950a8a67
Rafael Silva <rafa.306@hotmail.com>
2021-09-14 17:48:29 -0300

    Commit inicial

---
+++ src/main/java/com/silva/xpto/XptoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.silva.xpto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XptoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XptoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/silva/xpto/XptoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silva.xpto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XptoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

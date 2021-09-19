05bb0284d41a93ce0c97a8513def8be0fb69e403
Rafael Silva <rafa.306@hotmail.com>
2021-09-19 17:59:37 -0300

    Adicionado Lombok

--- src/main/java/com/silva/xpto/domain/model/Cliente.java
+++ src/main/java/com/silva/xpto/domain/model/Cliente.java
@@ -9,6 +9,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+	
+
+	@Data
+	@HashCodeAndEquals
 	@Entity(name = "cliente")
 	@Table(name = "cliente")
 	public class Cliente {
@@ -41,166 +46,10 @@ import javax.persistence.Table;
 	    
 	    private Date aberturaConta;
 	    
-	    public Cliente(Long id, String nome, String rua, String estado, String cpfCnpj, String bairro, String cidade,
-					Integer numero, String cep, String complemento, String telefone, Date dataCadastro,
-					Double saldoInicial, Date aberturaConta) {
-				super();
-				this.id = id;
-				this.nome = nome;
-				this.cpfCnpj = cpfCnpj;
-				this.bairro = bairro;
-				this.cidade = cidade;
-				this.estado = estado;
-				this.numero = numero;
-				this.rua = rua;
-				this.cep = cep;
-				this.complemento = complemento;
-				this.telefone = telefone;
-				this.dataCadastro = dataCadastro;
-				this.saldoInicial = saldoInicial;
-				this.aberturaConta = aberturaConta;
-			}
-	    
 	    public Cliente() {
 	    }
 
-		public Long getId() {
-			return id;
-		}
 
-		public void setId(Long id) {
-			this.id = id;
-		}
-
-		public String getNome() {
-			return nome;
-		}
-
-		public void setNome(String nome) {
-			this.nome = nome;
-		}
-
-		public String getCpfCnpj() {
-			return cpfCnpj;
-		}
-
-		public void setCpfCnpj(String cpfCnpj) {
-			this.cpfCnpj = cpfCnpj;
-		}
-
-		public String getBairro() {
-			return bairro;
-		}
-
-		public void setBairro(String bairro) {
-			this.bairro = bairro;
-		}
-
-		public String getCidade() {
-			return cidade;
-		}
-
-		public void setCidade(String cidade) {
-			this.cidade = cidade;
-		}
-
-		public Integer getNumero() {
-			return numero;
-		}
-
-		public void setNumero(Integer numero) {
-			this.numero = numero;
-		}
-
-		public String getCep(String cep) {
-			return cep;
-		}
-
-		public void setCep(String cep) {
-			this.cep = cep;
-		}
-
-		public String getComplemento() {
-			return complemento;
-		}
-
-		public void setComplemento(String complemento) {
-			this.complemento = complemento;
-		}
-
-		public String getTelefone() {
-			return telefone;
-		}
-
-		public void setTelefone(String telefone) {
-			this.telefone = telefone;
-		}
-
-		public Date getDataCadastro() {
-			return dataCadastro;
-		}
-
-		public void setDataCadastro(Date dataCadastro) {
-			this.dataCadastro = dataCadastro;
-		}
-
-		public Double getSaldoInicial() {
-			return saldoInicial;
-		}
-
-		public void setSaldoInicial(Double saldoInicial) {
-			this.saldoInicial = saldoInicial;
-		}
-
-		public Date getAberturaConta() {
-			return aberturaConta;
-		}
-
-		public void setAberturaConta(Date aberturaConta) {
-			this.aberturaConta = aberturaConta;
-		}
-		
-
-		public String getRua() {
-			return rua;
-		}
-
-		public void setRua(String rua) {
-			this.rua = rua;
-		}
-
-		public String getEstado() {
-			return estado;
-		}
-
-		public void setEstado(String estado) {
-			this.estado = estado;
-		}
-
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + ((id == null) ? 0 : id.hashCode());
-			return result;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Cliente other = (Cliente) obj;
-			if (id == null) {
-				if (other.id != null)
-					return false;
-			} else if (!id.equals(other.id))
-				return false;
-			return true;
-		}
 		
 	
 }
--- src/main/java/com/silva/xpto/domain/model/ContaBancaria.java
+++ src/main/java/com/silva/xpto/domain/model/ContaBancaria.java
@@ -7,9 +7,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity
-@Table(name = "conta_bancaria")
-public class ContaBancaria {
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+	
+	@Data
+	@Entity
+	@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+	@Table(name = "conta_bancaria")
+	public class ContaBancaria {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -27,70 +33,5 @@ public class ContaBancaria {
 	public ContaBancaria() {
 	}
 
-	public Long getIdContaBancaria() {
-		return idContaBancaria;
-	}
-
-	public void setIdContaBancaria(Long idContaBancaria) {
-		this.idContaBancaria = idContaBancaria;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getCodigoBanco() {
-		return codigoBanco;
-	}
-
-	public void setCodigoBanco(String codigoBanco) {
-		this.codigoBanco = codigoBanco;
-	}
-
-	public Integer getNumeroConta() {
-		return numeroConta;
-	}
-
-	public void setNumeroConta(Integer numeroConta) {
-		this.numeroConta = numeroConta;
-	}
-
-	public Integer getAgencia() {
-		return agencia;
-	}
-
-	public void setAgencia(Integer agencia) {
-		this.agencia = agencia;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((idContaBancaria == null) ? 0 : idContaBancaria.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ContaBancaria other = (ContaBancaria) obj;
-		if (idContaBancaria == null) {
-			if (other.idContaBancaria != null)
-				return false;
-		} else if (!idContaBancaria.equals(other.idContaBancaria))
-			return false;
-		return true;
-	}
-	
 	
 }
